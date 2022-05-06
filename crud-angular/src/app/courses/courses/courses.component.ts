068ac8df5e34dbc150539ad654dc737fc3d719e9
vinny <vinny.parker@gmail.com>
2022-05-06 20:09:40 -0300

    Ajustes no css da table; organizacao dos imports do material

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -10,7 +10,8 @@ export class CoursesComponent implements OnInit {
 
   coursesDataSource: Course[] = [
     { _id: 1, name: 'Angular', category:'front-end'},
-    { _id: 2, name: 'Spring Boot', category:'back-end'}
+    { _id: 2, name: 'Spring Boot', category:'back-end'},
+    { _id: 3, name: 'Spring Data JPA', category:'back-end'}
   ];
   displayedColumns = ['_id','name','category'];
 
